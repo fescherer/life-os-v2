bfb35350289bc4114b94ab4d84c2165e5b3d281c
unknown <felipescherer25@gmail.com>
2026-04-11 17:15:29 -0300

    :wrench: add settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import stylistic from '@stylistic/eslint-plugin'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'indent': ['error', 2],
+      '@stylistic/indent': ['error', 2],
+    }
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
