7bd59c3a451a2888defacc210a5db5409ea867e8
unknown <felipescherer25@gmail.com>
2026-04-11 17:59:29 -0300

    :wrench: add better eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,14 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import stylistic from '@stylistic/eslint-plugin'
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactPlugin from 'eslint-plugin-react';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  reactPlugin.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
   {
     plugins: {
       '@stylistic': stylistic
@@ -13,7 +17,9 @@ const eslintConfig = defineConfig([
     rules: {
       'indent': ['error', 2],
       '@stylistic/indent': ['error', 2],
-      "react/self-closing-comp": "error"
+      "react/self-closing-comp": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/no-multi-comp": ["error", { ignoreStateless: false }],
     }
   },
   globalIgnores([
